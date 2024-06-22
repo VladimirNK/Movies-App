e42134644c44857c31f58309f55b73e74c7462ea
Volodymyr <nk.kozlov@gmail.com>
2024-06-22 13:34:45 +0300

    Created the project

---
+++ Movies App/Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Vladimir Kozlov on 22.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
